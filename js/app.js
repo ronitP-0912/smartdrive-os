c9ef4d135cdef24492672d437f776eed94d6ddef
ronitP-0912 <ronitpaul.2012@gmail.com>
2026-06-05 03:45:42 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("SmartDrive OS Loaded");
+function updateClock(){
+
+const now = new Date();
+
+document.getElementById("clock").innerText =
+now.toLocaleTimeString([],{
+hour:'2-digit',
+minute:'2-digit'
+});
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
